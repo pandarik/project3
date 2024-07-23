102b8268edf5bca7d359516cb450185bb26cdc53
Jim Lao <lao_jim@yahoo.com>
2024-07-22 19:36:51 -0500

    updating files due to table merges

--- SQL Table Create.sql
+++ SQL Table Create.sql
@@ -1,7 +1,7 @@
-DROP TABLE IF EXISTS VRBO_dt;
+DROP TABLE IF EXISTS Vacation_Rental_DT;
 
-CREATE TABLE VRBO_dt(
-	Property_id VARCHAR, --make a unique id ourselves "Primary Key"
+CREATE TABLE Vacation_Rental_DT(
+	Property_id VARCHAR, 
 	Title VARCHAR,
 	URL VARCHAR,
 	Price INT,
@@ -12,37 +12,13 @@ CREATE TABLE VRBO_dt(
 	Rating VARCHAR,
 	Rating_desc VARCHAR,
 	Num_Reviews VARCHAR,	
-	Check_in_Date VARCHAR, --Fields have dates but will need to be changed
-	Check_out_Date VARCHAR, --Fields have dates but will need to be changed
+	Check_in_Date VARCHAR, 
+	Check_out_Date VARCHAR, 
 	Destination VARCHAR,
 	Room_type VARCHAR,
-	NumPeople VARCHAR, --need to do conversions in pandas to number for blanks
-	numbedroom VARCHAR, --need to do conversions in pandas to number
-	numbathroom VARCHAR, --need to do conversions in pandas to number
-	Source Varchar
-	);
-
-
-DROP TABLE IF EXISTS AirBNB_dt;
-
-CREATE TABLE AirBNB_dt(
-	Property_id VARCHAR, --make a unique id ourselves "Primary Key"
-	Title VARCHAR,
-	URL VARCHAR,
-	Price INT,
-	Total_price INT,
-	lat VARCHAR,
-	lng VARCHAR,
-	LatLong VARCHAR,
-	Rating VARCHAR,
-	Rating_desc VARCHAR,
-	Num_Reviews VARCHAR,	
-	Check_in_Date VARCHAR, --Fields are blank
-	Check_out_Date VARCHAR, --Fields are blank
-	Destination VARCHAR,
-	Room_type VARCHAR,
-	NumPeople VARCHAR, --need to do conversions in pandas to number for blanks
-	numbedroom VARCHAR, --need to do conversions in pandas to number
-	numbathroom VARCHAR, --need to do conversions in pandas to number
-	Source Varchar
-	);
+	NumPeople VARCHAR, 
+	numbedroom VARCHAR, 
+	numbathroom VARCHAR,
+	Source VARCHAR,
+	PRIMARY KEY(Source , Title, Property_id )
+);
